25bd3007035362da8bb0c38eca4ec7491f9b48b4
Sandro.Wüthrich <71585959+Sandrowue@users.noreply.github.com>
2023-03-27 15:06:21 +0300

    lintu lentää

--- kananlento_peli.py
+++ kananlento_peli.py
@@ -10,10 +10,21 @@ class Game:
         self.screen = pygame.display.set_mode((1000, 750)) 
         self.running = False  
         self.init_graphics()
+        self.init_objects()
 
     def init_graphics(self):
-        img_bird1 = pygame.image.load("images/bird/frame-1.png")
-        self.img_bird1 = pygame.transform. rotozoom(img_bird1, 0, 1/12)
+        self.bird_frame = 0
+        bird_imgs = [pygame.image.load(f"images/bird/frame-{i}.png")
+        for i in [1, 2, 3, 4, 5, 6, 7, 8]
+        ]
+
+        self.bird_imgs = [pygame.transform. rotozoom(x, 0, 1/12)
+        for x in bird_imgs]
+
+    def init_objects(self):
+        self.bird_y_speed = 0
+        self.bird_pos = (150, 100)
+        self.bird_lift = False
 
     def run(self):    
         clock = pygame.time.Clock()
@@ -30,16 +41,39 @@ class Game:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key in (pygame.K_SPACE, pygame.K_UP):
+                    self.bird_lift = True
+            elif event.type == pygame.KEYUP:
+                if event.key in (pygame.K_SPACE, pygame.K_UP):
+                    self.bird_lift = False
+
 
     def handle_game_logic(self):
-        pass
+
+        bird_y = self.bird_pos[1]
+        #Painovoima
+        if self.bird_lift:
+            self.bird_y_speed -= 0.5
+            self.bird_frame += 1
+        else:
+            self.bird_y_speed += 0.2
+        
+        bird_y += self.bird_y_speed
+        self.bird_pos = (self.bird_pos[0], bird_y)
 
     def update_screen(self):
         self.screen.fill("light blue")
         
 
         # piirä lintu
-        self.screen.blit(self.img_bird1, (100, 100))
+        angle = -90 * 0.04 * self.bird_y_speed
+        angle = max(min(angle, 75), -75)
+        
+        bird_img_i = self.bird_imgs[(self.bird_frame // 2) % 8]
+
+        bird_img = pygame.transform.rotozoom(bird_img_i, angle, 1)
+        self.screen.blit(bird_img, self.bird_pos)
 
         pygame.display.flip()
  
