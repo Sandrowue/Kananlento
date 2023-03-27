4fb0840355ab9d54812484f833bea765432f9ec5
Sandro.Wüthrich <71585959+Sandrowue@users.noreply.github.com>
2023-03-27 11:08:01 +0300

    lisäosat ja alku

---
+++ kananlento_peli.py
@@ -0,0 +1,27 @@
+import pygame
+
+def main():
+    screen = pygame.display.set_mode((1000, 750))  
+    clock = pygame.time.Clock()
+
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        
+        screen.fill("light blue")
+        pygame.display.flip()
+
+        clock.tick(60) # Odota niin kauan, että ruudun päivitysnopeus on 60fps
+        
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
